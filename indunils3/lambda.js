e01f57eb821b89bb5bec0fc54650303156025127
Indunil1992 <indunil@adroitlogic.com>
2019-07-11 18:31:11 +0530

    x

--- indunils3/lambda.js
+++ indunils3/lambda.js
@@ -5,7 +5,7 @@ const sqs = new SL_AWS.SQS(AWS);
 exports.handler = function (event, context, callback) {
     sqs.receiveMessage({
         QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/sample`,
-        AttributeNames: ['All'],
+        AttributeNames: ['ApproximateReceiveCount'],
         MaxNumberOfMessages: '3',
         VisibilityTimeout: '30',
         WaitTimeSeconds: '0',
@@ -15,20 +15,20 @@ exports.handler = function (event, context, callback) {
             if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
                 let receivedMessages = receivedMsgData.Messages;
                 receivedMessages.forEach(message => {
-                     console.log("reccc message");
-                 console.log( message );
+                    console.log("reccc message");
+                    console.log(message);
                     // your logic to access each message through out the loop. Each message is available under variable message 
                     // within this block
                 });
             } else {
-                 console.log("Success");
-                 //console.log( data );
+                console.log("Success");
+                //console.log( data );
                 // No messages to process
             }
         })
         .catch(err => {
-             console.log("not Success");
-                 console.log( err );
+            console.log("not Success");
+            console.log(err);
             // error handling goes here
         });
 
