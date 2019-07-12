f5b81ff725fa068caf6b67d206a8a637d1d0c2e5
Indunil1992 <indunil@adroitlogic.com>
2019-07-12 18:03:30 +0530

    cd

--- indunils3/lambda.js
+++ indunils3/lambda.js
@@ -1,34 +1,33 @@
 let AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
-
 exports.handler = function (event, context, callback) {
-    s3.putObject({
-        "Body": "test",
-        "Bucket": "indunil1",
-        "Key": "a1",
-        "ServerSideEncryption": "AES256",
-        "ACL": "private",
-        "Metadata": {
-            "metadata1": "1",
-            "metadata2": "2"
-        },
-        "Tagging": "t5=555"
-    })
-        .promise()
+    s3.getObject({
+        'Bucket': "indunil1",
+        'Key': "1"
+    }).promise()
         .then(data => {
+            console.log("Success");
+                 console.log( data );
             console.log(data);           // successful response
             /*
             data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
+                AcceptRanges: "bytes", 
+                ContentLength: 3191, 
+                ContentType: "image/jpeg", 
+                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"", 
+                LastModified: "<Date Representation>", 
+                Metadata: {}, 
+                TagCount: 2, 
+                VersionId: "null"
             }
             */
         })
         .catch(err => {
+            console.log("not Success");
+                 console.log( err );
             console.log(err, err.stack); // an error occurred
         });
-
 
 
     callback(null, { "message": "Successfully put executed" });
