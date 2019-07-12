fe5fad2f94b5aeda7d6a186953cd7975b3c3b084
Indunil1992 <indunil@adroitlogic.com>
2019-07-12 16:51:44 +0530

    ww

--- indunils3/lambda.js
+++ indunils3/lambda.js
@@ -3,18 +3,19 @@ const s3 = new AWS.S3();
 
 
 exports.handler = function (event, context, callback) {
-
     s3.putObject({
-        "Body": "testrun",
+        "Body": "test",
         "Bucket": "indunil1",
-        "Key": "induobject"
+        "Key": "a1",
+        "Tagging": "tk4=345",
+        "Metadata": {
+            "metadata1": "1",
+            "metadata2": "2"
+        }
     })
         .promise()
         .then(data => {
-            console.log(data);      
-            console.log("Success");
-                 console.log( data );
-                 // successful response
+            console.log(data);           // successful response
             /*
             data = {
                 ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
@@ -23,8 +24,6 @@ exports.handler = function (event, context, callback) {
             */
         })
         .catch(err => {
-            console.log("not Success");
-                 console.log( err );
             console.log(err, err.stack); // an error occurred
         });
 
