880f344547d8159b812365e9f6fac4ae054f248b
Indunil1992 <indunil@adroitlogic.com>
2019-07-12 09:52:10 +0530

    x

--- indunils3/lambda.js
+++ indunils3/lambda.js
@@ -4,35 +4,34 @@ const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
     sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/sample`,
-        AttributeNames: ['ApproximateReceiveCount'],
-        MaxNumberOfMessages: '3',
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
         VisibilityTimeout: '30',
         WaitTimeSeconds: '0',
-        MessageAttributeNames: ['1', '2', '3', '4']
+        MessageAttributeNames: ['as', 'ad']
     }).promise()
         .then(receivedMsgData => {
             if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
                 let receivedMessages = receivedMsgData.Messages;
                 receivedMessages.forEach(message => {
-                    console.log("reccc message");
-                    console.log(message);
+                    console.log("msg Success");
+                 console.log( message );
                     // your logic to access each message through out the loop. Each message is available under variable message 
                     // within this block
                 });
             } else {
-                console.log("Success");
-                //console.log( data );
+                console.log("no msg to Success");
+                 //console.log( data );
                 // No messages to process
             }
         })
         .catch(err => {
-            console.log("not Success");
-            console.log(err);
+            console.log("err Success");
+                 console.log( err );
             // error handling goes here
         });
 
 
-
-    callback(null, { "message": "Successfully 33 executed" });
+    callback(null, { "message": "Successfully 55 executed" });
 }
