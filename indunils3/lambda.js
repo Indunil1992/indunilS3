375e3256cece8b53772897ee423f6ef275538699
Indunil1992 <indunil@adroitlogic.com>
2019-07-14 20:32:45 +0530

    vfs

--- indunils3/lambda.js
+++ indunils3/lambda.js
@@ -3,37 +3,22 @@ const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
 
-
-    s3.listObjects({
-        'Bucket': 'sample.indunil',
-        'MaxKeys': 10,
-        'Prefix': '1'
+    s3.getBucketLocation({
+        'Bucket': "hiru.test123"
     }).promise()
         .then(data => {
+             console.log("successful response"); 
             console.log(data);           // successful response
             /*
             data = {
-                Contents: [
-                    {
-                       ETag: "\"70ee1738b6b21e2c8a43f3a5ab0eee71\"",
-                       Key: "example1.jpg",
-                       LastModified: "<Date Representation>",
-                       Owner: {
-                          DisplayName: "myname",
-                          ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
-                       },
-                       Size: 11,
-                       StorageClass: "STANDARD"
-                    },
-                    // {...}
-                ]
+                LocationConstraint: "us-west-2"
             }
             */
         })
         .catch(err => {
+                console.log("an error occurred"); 
             console.log(err, err.stack); // an error occurred
         });
-
 
 
 
