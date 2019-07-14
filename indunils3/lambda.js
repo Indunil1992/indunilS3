6d022cb1945c35b90a18ffbcb6f9a26373672b1b
Indunil1992 <indunil@adroitlogic.com>
2019-07-14 06:33:34 +0530

    io

--- indunils3/lambda.js
+++ indunils3/lambda.js
@@ -2,32 +2,31 @@ let AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
-    s3.getObject({
-        'Bucket': "indunil1",
-        'Key': "1.xlsx"
+
+
+    s3.copyObject({
+        'Bucket': "hiru.test123",
+        'CopySource': `/indunil1/11111`,
+        'Key': "11111"
     }).promise()
         .then(data => {
-            console.log("Success");
-            console.log(data);
+
+            console.log("successful response");
             console.log(data);           // successful response
             /*
             data = {
-                AcceptRanges: "bytes", 
-                ContentLength: 3191, 
-                ContentType: "image/jpeg", 
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"", 
-                LastModified: "<Date Representation>", 
-                Metadata: {}, 
-                TagCount: 2, 
-                VersionId: "null"
+                CopyObjectResult: {
+                    ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
+                    LastModified: "<Date Representation>"
+                }
             }
             */
         })
         .catch(err => {
-            console.log("not Success");
-            console.log(err);
+              console.log("not successful response");
             console.log(err, err.stack); // an error occurred
         });
+
 
 
     callback(null, { "message": "Successfully put executed" });
