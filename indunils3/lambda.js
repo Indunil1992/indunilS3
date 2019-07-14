210b8025a2f5145340a980ee63fd6429387f4690
Indunil1992 <indunil@adroitlogic.com>
2019-07-14 21:16:14 +0530

    123123

--- indunils3/lambda.js
+++ indunils3/lambda.js
@@ -4,10 +4,10 @@ const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
 
     s3.getBucketLocation({
-        'Bucket': "hiru.test123"
+        'Bucket': "indunil1"
     }).promise()
         .then(data => {
-             console.log("successful response"); 
+            console.log("successful response");
             console.log(data);           // successful response
             /*
             data = {
@@ -16,11 +16,11 @@ exports.handler = function (event, context, callback) {
             */
         })
         .catch(err => {
-                console.log("an error occurred"); 
+            console.log("an error occurred");
             console.log(err, err.stack); // an error occurred
         });
 
 
 
-    callback(null, { "message": "Successfully copyyy executed" });
+    callback(null, { "message": "Successfully getlocation executed" });
 }
