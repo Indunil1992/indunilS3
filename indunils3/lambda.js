2cfc994b6990b5d37289ba5cf823405cf2e2038c
Indunil1992 <indunil@adroitlogic.com>
2019-07-14 07:46:36 +0530

    io

--- indunils3/lambda.js
+++ indunils3/lambda.js
@@ -26,5 +26,5 @@ exports.handler = function (event, context, callback) {
 
 
 
-    callback(null, { "message": "Successfully s3 executed" });
+    callback(null, { "message": "Successfully t executed" });
 }
