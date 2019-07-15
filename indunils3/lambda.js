bcf3fdba99e234d11165d0fe2c4c74282419c7af
Indunil1992 <indunil@adroitlogic.com>
2019-07-15 09:56:07 +0530

    qw

--- indunils3/lambda.js
+++ indunils3/lambda.js
@@ -1,30 +1,6 @@
-let AWS = require('aws-sdk');
-const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
 
-    s3.putObject({
-        "Body": "1234567890",
-        "Bucket": "indunil1",
-        "Key": "142",
-        "ACL": "private"
-    })
-        .promise()
-        .then(data => {
-            console.log("successful response");
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log("an error occurred");
-            console.log(err, err.stack); // an error occurred
-        });
-
-
+    
     callback(null, { "message": "Successfully getlocation executed" });
 }
