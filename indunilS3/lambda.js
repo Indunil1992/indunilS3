bfc1ea102b892b7a4d9c3a73eccf5e42ae371a48
Indunil1992 <indunil@adroitlogic.com>
2019-07-15 15:06:00 +0530

    1111111111111

--- indunilS3/lambda.js
+++ indunilS3/lambda.js
@@ -2,41 +2,14 @@ let AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
-
-    s3.putObject({
-        "Body": "1",
-        "Bucket": "indunil3",
-        "Key": "1",
-        "ACL": "bucket-owner-read"
-    })
-        .promise()
+    s3.deleteObject({
+        'Bucket': "indunil1",
+        'Key': "1"
+    }).promise()
         .then(data => {
             console.log(data);           // successful response
             /*
-            data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
-
-    s3.putObject({
-        "Body": "3",
-        "Bucket": "indunil4",
-        "Key": "3",
-        "ACL": "bucket-owner-full-control"
-    })
-        .promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
+                data = {}
             */
         })
         .catch(err => {
@@ -45,5 +18,8 @@ exports.handler = function (event, context, callback) {
 
 
 
-    callback(null, { "message": "Successfully executed" });
+
+
+
+    callback(null, { "message": "Successfully executed 888" });
 }
