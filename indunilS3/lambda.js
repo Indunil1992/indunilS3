e2574509800a304a284646febd9f5a239cc59af3
Indunil1992 <indunil@adroitlogic.com>
2019-07-15 14:34:37 +0530

    7890

--- indunilS3/lambda.js
+++ indunilS3/lambda.js
@@ -144,6 +144,46 @@ exports.handler = function (event, context, callback) {
             console.log(err, err.stack); // an error occurred
         });
 
+    s3.putObject({
+        "Body": "3",
+        "Bucket": "indunil3",
+        "Key": "3",
+        "ACL": "bucket-owner-read"
+    })
+        .promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
+                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
+            }
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
+    s3.putObject({
+        "Body": "5",
+        "Bucket": "indunil4",
+        "Key": "5",
+        "ACL": "bucket-owner-full-control"
+    })
+        .promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
+                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
+            }
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
+
+
 
 
     callback(null, { "message": "Successfully executed" });
