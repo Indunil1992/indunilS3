3e1d4685327459f0bcd3b20d6200cd1b4bf84e35
Indunil1992 <indunil@adroitlogic.com>
2019-07-15 18:06:00 +0530

    vfr

--- indunilS3/lambda.js
+++ indunilS3/lambda.js
@@ -1,40 +1,23 @@
 let AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+const sns = new AWS.SNS();
+
 
 exports.handler = function (event, context, callback) {
 
-    s3.listObjects({
-        'Bucket': 'indunil1',
-        'MaxKeys': 10,
-        'Prefix': ''
+    sns.publish({
+        Message: 'hiii',
+        Subject: '123',
+        MessageAttributes: {},
+        MessageStructure: 'String',
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
     }).promise()
         .then(data => {
-             console.log("success"); 
-            console.log(data);           // successful response
-            /*
-            data = {
-                Contents: [
-                    {
-                       ETag: "\"70ee1738b6b21e2c8a43f3a5ab0eee71\"",
-                       Key: "example1.jpg",
-                       LastModified: "<Date Representation>",
-                       Owner: {
-                          DisplayName: "myname",
-                          ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
-                       },
-                       Size: 11,
-                       StorageClass: "STANDARD"
-                    },
-                    // {...}
-                ]
-            }
-            */
+            // your code goes here
         })
         .catch(err => {
-
-             console.log("not success"); 
-            console.log(err, err.stack); // an error occurred
+            // error handling goes here
         });
+
 
 
     callback(null, { "message": "Successfully executed puooo" });
