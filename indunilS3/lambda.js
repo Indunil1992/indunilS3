6a6e4f7d3773b14b95938a436fc81d6ca0ae0f74
Indunil1992 <indunil@adroitlogic.com>
2019-07-15 15:53:06 +0530

    koplp

--- indunilS3/lambda.js
+++ indunilS3/lambda.js
@@ -1,49 +1,8 @@
-let AWS = require('aws-sdk');
-const s3 = new AWS.S3();
-
 exports.handler = function (event, context, callback) {
 
-    s3.putObject({
-        "Body": "142",
-        "Bucket": "indunil1",
-        "Key": "142",
-        "ACL": "public-read"
-    })
-        .promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
-
-    s3.putObject({
-        "Body": "733",
-        "Bucket": "indunil1",
-        "Key": "733",
-        "ACL": "public-read-write"
-    })
-        .promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
+ 
 
 
 
-    callback(null, { "message": "Successfully executed 888" });
+    callback(null, { "message": "Successfully executed dfdf" });
 }
