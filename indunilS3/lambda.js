bb5a5c87d050132f0c3f8e7f99be4c50d5653eb8
Indunil1992 <indunil@adroitlogic.com>
2019-07-15 15:41:17 +0530

    890

--- indunilS3/lambda.js
+++ indunilS3/lambda.js
@@ -2,40 +2,46 @@ let AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
-    s3.deleteObject({
-        'Bucket': "indunil1",
-        'Key': "2"
-    }).promise()
+
+    s3.putObject({
+        "Body": "142",
+        "Bucket": "indunil1",
+        "Key": "142",
+        "ACL": "public-read"
+    })
+        .promise()
         .then(data => {
-            console.log(data);
-            console.log("successful response 1");      // successful response
+            console.log(data);           // successful response
             /*
-                data = {}
+            data = {
+                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
+                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
+            }
             */
         })
         .catch(err => {
-            console.log(err, err.stack);
-            console.log("not successful response 1");
-            // an error occurred
+            console.log(err, err.stack); // an error occurred
         });
 
-    s3.deleteObject({
-        'Bucket': "indunil1",
-        'Key': "10"
-    }).promise()
+    s3.putObject({
+        "Body": "733",
+        "Bucket": "indunil1",
+        "Key": "733",
+        "ACL": "public-read-write"
+    })
+        .promise()
         .then(data => {
-            console.log(data);
-            console.log("successful response 2");           // successful response
+            console.log(data);           // successful response
             /*
-                data = {}
+            data = {
+                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
+                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
+            }
             */
         })
         .catch(err => {
-            console.log(err, err.stack);
-            console.log("not successful response 2");  // an error occurred
+            console.log(err, err.stack); // an error occurred
         });
-
-
 
 
 
