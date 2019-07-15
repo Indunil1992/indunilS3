6e0300127015975795421def5692cd337856cab2
Indunil1992 <indunil@adroitlogic.com>
2019-07-15 14:54:41 +0530

    iooi

--- indunilS3/lambda.js
+++ indunilS3/lambda.js
@@ -3,132 +3,11 @@ const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
 
-
-    s3.putObject({
-        "Body": "2",
-        "Bucket": "indunil1",
-        "Key": "2",
-        "ACL": "public-read"
-    })
-        .promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
-
-    s3.putObject({
-        "Body": "3",
-        "Bucket": "indunil1",
-        "Key": "3",
-        "ACL": "public-read-write"
-    })
-        .promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
-
-    s3.putObject({
-        "Body": "4",
-        "Bucket": "indunil1",
-        "Key": "4",
-        "ACL": "authenticated-read"
-    })
-        .promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
-
-    s3.putObject({
-        "Body": "5",
-        "Bucket": "indunil1",
-        "Key": "5",
-        "ACL": "aws-exec-read"
-    })
-        .promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
-
-    s3.putObject({
-        "Body": "6",
-        "Bucket": "indunil1",
-        "Key": "6",
-        "ACL": "bucket-owner-read"
-    })
-        .promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
-
-
-    s3.putObject({
-        "Body": "7",
-        "Bucket": "indunil1",
-        "Key": "7",
-        "ACL": "bucket-owner-full-control"
-    })
-        .promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
     s3.putObject({
         "Body": "1",
-        "Bucket": "indunil1",
+        "Bucket": "indunil3",
         "Key": "1",
-        "ACL": "private"
+        "ACL": "bucket-owner-read"
     })
         .promise()
         .then(data => {
@@ -146,29 +25,8 @@ exports.handler = function (event, context, callback) {
 
     s3.putObject({
         "Body": "3",
-        "Bucket": "indunil3",
-        "Key": "3",
-        "ACL": "bucket-owner-read"
-    })
-        .promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); 
-            console.log("333333333");
-            // an error occurred
-        });
-    s3.putObject({
-        "Body": "5",
         "Bucket": "indunil4",
-        "Key": "5",
+        "Key": "3",
         "ACL": "bucket-owner-full-control"
     })
         .promise()
@@ -182,10 +40,8 @@ exports.handler = function (event, context, callback) {
             */
         })
         .catch(err => {
-            console.log(err, err.stack); 
-            console.log("77777777777777");// an error occurred
+            console.log(err, err.stack); // an error occurred
         });
-
 
 
 
