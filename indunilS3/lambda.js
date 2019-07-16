4a32b8e7f7dbce36bb7c9a522582a8d2728035dc
Indunil1992 <indunil@adroitlogic.com>
2019-07-16 08:43:35 +0530

    cdf

--- indunilS3/lambda.js
+++ indunilS3/lambda.js
@@ -5,16 +5,23 @@ const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
 
     sns.publish({
-        Message: 'hiii',
-        Subject: '123',
-        MessageAttributes: {},
+        Message: 'hi sns ',
+        Subject: 'sub sns',
+        MessageAttributes: {
+            '1': {
+                'DataType': 'String',
+                'StringValue': 'abc'
+            }
+        },
         MessageStructure: 'String',
         TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
     }).promise()
         .then(data => {
+            console.log("yes");
             // your code goes here
         })
         .catch(err => {
+            console.log("no");
             // error handling goes here
         });
 
