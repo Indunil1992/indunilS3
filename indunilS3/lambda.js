c802c0988ea22abb420a1ec85497f4f6e235aeb8
Indunil1992 <indunil@adroitlogic.com>
2019-07-16 23:55:02 +0530

    jkh

--- indunilS3/lambda.js
+++ indunilS3/lambda.js
@@ -26,5 +26,5 @@ exports.handler = function (event, context, callback) {
 
 
 
-    callback(null, { "message": "Successfully executed re" });
+    callback(null, { "message": "Successfully executed rrr" });
 }
