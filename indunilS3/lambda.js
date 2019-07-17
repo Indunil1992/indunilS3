fd1605b1a225f396f4b1de6fc20c4ce9ade10f71
Indunil1992 <indunil@adroitlogic.com>
2019-07-17 10:53:23 +0530

    jopi

--- indunilS3/lambda.js
+++ indunilS3/lambda.js
@@ -1,7 +1,22 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
 
 exports.handler = function (event, context, callback) {
 
-   
+    sns.publish({
+        Message: 'msg sns',
+        Subject: 'sub sns',
+        MessageAttributes: {},
+        MessageStructure: 'String',
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:indunil-topic'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
 
 
     callback(null, { "message": "Successfully executed rrr" });
