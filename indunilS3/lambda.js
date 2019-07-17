e6ecdaa5f8ddba358eea66257d739d636eaa84e2
Indunil1992 <indunil@adroitlogic.com>
2019-07-17 18:02:11 +0530

    iopu

--- indunilS3/lambda.js
+++ indunilS3/lambda.js
@@ -5,36 +5,33 @@ const sqs = new SL_AWS.SQS(AWS);
 exports.handler = function (event, context, callback) {
 
     sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/test-queue.fifo`,
         AttributeNames: ['All'],
-        MaxNumberOfMessages: '10',
+        MaxNumberOfMessages: '1',
         VisibilityTimeout: '30',
-        WaitTimeSeconds: '10',
-        MessageAttributeNames: ['a']
+        WaitTimeSeconds: '0',
+        MessageAttributeNames: ['abs']
     }).promise()
         .then(receivedMsgData => {
             if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
                 let receivedMessages = receivedMsgData.Messages;
                 receivedMessages.forEach(message => {
-                    console.log("msgeeee");
-                     console.log("receivedMessages");
-                     console.log("message");
-                      console.log("receivedMsgData");
-                      console.log("receivedMsgData.Messages");
-
+                    console.log("Success");
+                 console.log( data );
                     // your logic to access each message through out the loop. Each message is available under variable message 
                     // within this block
                 });
             } else {
-                console.log("no  msgeeee");
+                console.log("no msg");
+                 console.log( data );
                 // No messages to process
             }
         })
         .catch(err => {
-            console.log("errr msgeeee");
+            console.log("err err");
+                 console.log( data );
             // error handling goes here
         });
-
 
 
     callback(null, { "message": "Successfully executed SQS" });
