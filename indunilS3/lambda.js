ceb7d85d02ba2e79b2ecee102a6bd9ea35a131a1
Indunil1992 <indunil@adroitlogic.com>
2019-07-25 13:13:40 +0530

    ll

--- indunilS3/lambda.js
+++ indunilS3/lambda.js
@@ -1,38 +1,7 @@
-let AWS = require('aws-sdk');
-let SL_AWS = require('slappforge-sdk-aws');
-const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
 
-    sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/test`,
-        AttributeNames: ['MessageGroupId'],
-        MaxNumberOfMessages: '10',
-        VisibilityTimeout: '80',
-        WaitTimeSeconds: '10',
-        MessageAttributeNames: ['abs']
-    }).promise()
-        .then(receivedMsgData => {
-            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
-                let receivedMessages = receivedMsgData.Messages;
-                receivedMessages.forEach(message => {
-                    console.log("Success");
-                    console.log(data);
-                    // your logic to access each message through out the loop. Each message is available under variable message 
-                    // within this block
-                });
-            } else {
-                console.log("no msg");
-                console.log(data);
-                // No messages to process
-            }
-        })
-        .catch(err => {
-            console.log("err err");
-            console.log(data);
-            // error handling goes here
-        });
-
+   
 
     callback(null, { "message": "Successfully executed SQS" });
 }
